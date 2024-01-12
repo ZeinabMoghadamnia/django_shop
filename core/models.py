09a9046b5a0d19ed0e338637f8c1e121f9af4979
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-12 21:19:34 +0330

    [FinalProject][#3]|product app created and category and product models have been added

--- core/models.py
+++ core/models.py
@@ -6,5 +6,8 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(auto_now=True)
+    restored_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
     class Meta:
         abstract = True
