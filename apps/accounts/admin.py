a9aa8cf11bb225880a87bacb0e7e6a66d24aaa6b
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-13 18:33:05 +0330

    [FinalProject][#4]|some fields updated

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Member
+from .models import User, Address, OtpCode
 
 # Register your models here.
 
-admin.site.register(Member)
+admin.site.register([User, Address, OtpCode])
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,65 +1,70 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
-from django.contrib.auth.base_user import BaseUserManager
+# from django.utils.translation import gettext_lazy as _
+# from social_core.backends import username
+
 from ..core.models import BaseModel
-
-class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
-    def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given email and password.
-        """
-        if not email:
-            raise ValueError("The Email must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        return self.create_user(email, password, **extra_fields)
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 
-class CustomerUser(AbstractUser, BaseModel):
-    CUSTOMERUSER_EMPLOYEE = 'e'
-    CUSTOMERUSER_CUSTOMER = 'c'
-    CUSTOMERUSER_MANAGER = 'm'
-    CUSTOMERUSER_STATUS = (
-        (CUSTOMERUSER_EMPLOYEE,'employee'),
-        (CUSTOMERUSER_CUSTOMER,'customer'),
-        (CUSTOMERUSER_MANAGER,'manager')
-    )
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
-    birth_day = models.DateField(null=True, blank=True)
-    mobile_regex = RegexValidator(regex='^(\+98|0)?9\d{9}$', message="Phone number must be entered in the format: '+989199999933'.")
-    phone_number = models.CharField(validators=[mobile_regex], max_length=20, unique=True)
-    user_type = models.CharField(max_length=1,choices=CUSTOMERUSER_STATUS, default=CUSTOMERUSER_CUSTOMER)
-    city = models.CharField(max_length=30, null=True, blank=True)
-    address = models.CharField(max_length=150, null=True, blank=True)
-    image = models.ImageField(upload_to='covers/', blank=True)
+# from .managers import MyUserManager
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
-    # user_create = CustomUserManager()
+class User(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True)
+    phone_regex = RegexValidator(regex='^(\+98|0)?9\d{9}$', message="Phone number must be entered in the format: '+989199999933'.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=11, unique=True)
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    date_of_birth = models.DateField(null=True, blank=True)
+    image = models.ImageField(upload_to='users_profile_pics/', null=True, blank=True)
+    groups = models.ManyToManyField(Group, related_name='user_accounts')
+    user_permissions = models.ManyToManyField(Permission, related_name='user_accounts_permissions')
 
-    objects = CustomUserManager()
+    # USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone', 'first_name', 'last_name', 'email']
 
+    # class Meta:
+    #     verbose_name = _('User')
+    #     verbose_name_plural = _('Users')
+
+    def __str__(self):
+        return self.email
+
+    # def has_perm(self, perm, obj=None):
+    #     "Does the user have a specific permission?"
+    #     # Simplest possible answer: Yes, always
+    #     return True
+    #
+    # def has_module_perms(self, app_label):
+    #     "Does the user have permissions to view the app `app_label`?"
+    #     # Simplest possible answer: Yes, always
+    #     return True
+    #
+    # # @property
+    # def is_staff(self):
+    #     "Is the user a member of staff?"
+    #     # Simplest possible answer: All admins are staff
+    #     return self.is_admin
+
+    # def save(self, *args, **kwargs):
+    #     self.username = self.email
+    #     super().save(*args, **kwargs)
+
+
+class Address(BaseModel):
+    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    City = models.CharField(max_length=30)
+    province = models.CharField(max_length=30)
+    complete_address = models.TextField(max_length=200)
+    # home_plate = models.CharField(max_length=20)
+    # postal_code = models.CharField(max_length=20)
+
+
+class OtpCode(models.Model):
+    phone_number = models.CharField(max_length=11)
+    code = models.PositiveSmallIntegerField()
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.phone_number} - {self.code} - {self.created}'
