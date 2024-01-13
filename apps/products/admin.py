47cf44b0e554f2a2dcfe363f028eb9937d004d6f
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-13 16:11:16 +0330

    [FinalProject][#5]|The models were registered in the admin panel

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Product, Discount, Image, Comment, Like
 
 # Register your models here.
+
+admin.site.register([Category, Product, Discount, Image, Comment])
