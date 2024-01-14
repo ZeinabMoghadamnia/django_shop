8faa80bc12cdd90d854b3bca328c926612d46684
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-14 18:09:12 +0330

    [FinalProject][#1]|locale for product app updated

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
-    is_deleted = models.BooleanField(default=False)
-    deleted_at = models.DateTimeField(auto_now=True)
-    restored_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True, verbose_name=_('date created'))
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name=_('date updated'))
+    is_deleted = models.BooleanField(default=False, verbose_name=_('delete status'))
+    deleted_at = models.DateTimeField(auto_now=True, blank=True, null=True, verbose_name=_('date deleted'))
+    restored_at = models.DateTimeField(auto_now=True, blank=True, null=True , verbose_name=_('date restored'))
+    is_active = models.BooleanField(default=True, verbose_name=_('active status'))
     class Meta:
         abstract = True
