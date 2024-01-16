6016b6045165846796d89f46a5b4fdd55707187b
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-16 11:58:42 +0330

    [FinalProject][#3]|auto create slug added

--- apps/products/tests/test_models.py
+++ apps/products/tests/test_models.py
@@ -4,13 +4,17 @@ from ..models import Discount, Brand, Category, Product
 class DiscountTestCase(TestCase):
     def setUp(self):
         self.discount_percentage = Discount.objects.create(code='Test DiscountP', discount_type='percentage', value=10)
-        self.discount_amount = Discount.objects.create(code='Test DiscountA', discount_type='amount', value=1000)
+        self.discount_amount = Discount.objects.create(code='Test DiscountA', discount_type='amount', value=100)
 
 
-    def test_discount_model(self):
+    def test_discount_p_model(self):
         discount = Discount.objects.get(code='Test DiscountP')
         self.assertEqual(discount.discount_type, 'percentage')
         self.assertEqual(discount.value, 10)
+    def test_discount_a_model(self):
+        discount = Discount.objects.get(code='Test DiscountA')
+        self.assertEqual(discount.discount_type, 'amount')
+        self.assertEqual(discount.value, 100)
 
 
 class BrandTestCase(TestCase):
@@ -31,27 +35,42 @@ class CategoryTestCase(TestCase):
 
 class ProductTestCase(TestCase):
     def setUp(self):
-        self.product = Product.objects.create(
+        self.product1 = Product.objects.create(
             name='Test Product1',
             price=1000,
             quantity=10,
-            main_image=None,  # این بخش را با تصویر معتبر تغییر دهید
+            main_image=None,
             category=Category.objects.create(name='Test Category', slug='test-category'),
             brand=Brand.objects.create(name='Test Brand', slug='test-brand'),
-            discount=Discount.objects.create(code='Test DiscountP', discount_type='percentage', value=20),
-            discounted_price=None  # این بخش را با مقدار مناسب تغییر دهید
+            discount=Discount.objects.create(code='Test DiscountP', discount_type='percentage', value=50),
+            discounted_price=None
         )
-        # self.product2 = Product.objects.create(
-        #     name='Test Product2',
-        #     price=10000,
-        #     quantity=10,
-        # )
-    def test_product_model(self):
-        product = Product.objects.get(name='Test Product1')
-        self.assertEqual(product.name, 'Test Product1')
-        self.assertEqual(product.price, 1000)
-        self.assertEqual(product.quantity, 10)
-        self.assertEqual(product.category.name, 'Test Category')  # تغییر در اینجا
-        self.assertEqual(product.brand.name, 'Test Brand')  # تغییر در اینجا
-        self.assertEqual(product.discount.code, 'Test DiscountP')  # تغییر در اینجا
+        self.product2 = Product.objects.create(
+            name='Test Product2',
+            price=1000,
+            quantity=10,
+            main_image=None,
+            category=Category.objects.create(name='Test Category2', slug='test-category-2'),
+            brand=Brand.objects.create(name='Test Brand2', slug='test-brand-2'),
+            discount=Discount.objects.create(code='Test DiscountA', discount_type='amount', value=100),
+            discounted_price=None
+        )
+    def test_product1_model(self):
+        product1 = Product.objects.get(name='Test Product1')
+        self.assertEqual(product1.name, 'Test Product1')
+        self.assertEqual(product1.price, 1000)
+        self.assertEqual(product1.quantity, 10)
+        self.assertEqual(product1.category.name, 'Test Category')
+        self.assertEqual(product1.brand.name, 'Test Brand')
+        self.assertEqual(product1.discount.code, 'Test DiscountP')
+        self.assertEqual(product1.discounted_price, 500)
+    def test_product2_model(self):
+        product2 = Product.objects.get(name='Test Product2')
+        self.assertEqual(product2.name, 'Test Product2')
+        self.assertEqual(product2.price, 1000)
+        self.assertEqual(product2.quantity, 10)
+        self.assertEqual(product2.category.name, 'Test Category2')
+        self.assertEqual(product2.brand.name, 'Test Brand2')
+        self.assertEqual(product2.discount.code, 'Test DiscountA')
+        self.assertEqual(product2.discounted_price, 900)
 
