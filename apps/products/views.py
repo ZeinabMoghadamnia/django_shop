0d176f9ca37f8c0fbf4cfe9fa7cdbebfa809ebdf
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-16 23:53:54 +0330

    [FinalProject][#3]|description added

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Product
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'base.html'
+class IndexView(ListView):
+    model = Product
+    template_name = 'products/home.html'
+    context_object_name = 'products'
+    queryset = Product.objects.prefetch_related('image').order_by('id')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
