be067b9a8a6ce484ae86f70a91d676a34f5ed57e
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-18 02:57:12 +0330

    [FinalProject][#3]|tests updated

--- apps/products/tests/test_models.py
+++ apps/products/tests/test_models.py
@@ -1,26 +1,25 @@
 # aaaaaaaaaaa
 from django.test import TestCase
-from ..models import Discount, Brand, Category, Product
+from ..models import Discount, Brand, Category, Product, Comment, Like
+from ...accounts.models import User
 
 
 class DiscountTestCase(TestCase):
     def setUp(self):
-        self.discount_percentage = Discount.objects.create(code='Test DiscountP', discount_type='percentage', value=10,
-                                                           slug='test-discountp')
-        self.discount_amount = Discount.objects.create(code='Test DiscountA', discount_type='amount',
-                                                       value=100, slug='test-discounta')
+        self.discount_percentage = Discount.objects.create(code='Test DiscountP', discount_type='percentage', value=10)
+        self.discount_amount = Discount.objects.create(code='Test DiscountA', discount_type='amount', value=100)
 
-        def test_discount_p_model(self):
-            discount = Discount.objects.get(code='Test DiscountP')
-            self.assertEqual(discount.discount_type, 'percentage')
-            self.assertEqual(discount.value, 10)
-            self.assertEqual(discount.slug, 'test-discountp')
+    def test_discount_p_model(self):
+        discount = Discount.objects.get(code='Test DiscountP')
+        self.assertEqual(discount.discount_type, 'percentage')
+        self.assertEqual(discount.value, 10)
+        self.assertEqual(discount.code, 'Test DiscountP')
 
-        def test_discount_a_model(self):
-            discount = Discount.objects.get(code='Test DiscountA')
-            self.assertEqual(discount.discount_type, 'amount')
-            self.assertEqual(discount.value, 100)
-            self.assertEqual(discount.slug, 'test-discounta')
+    def test_discount_a_model(self):
+        discount = Discount.objects.get(code='Test DiscountA')
+        self.assertEqual(discount.discount_type, 'amount')
+        self.assertEqual(discount.value, 100)
+        self.assertEqual(discount.code, 'Test DiscountA')
 
 
 class BrandTestCase(TestCase):
@@ -69,8 +68,20 @@ class ProductTestCase(TestCase):
             price=1000,
             quantity=10,
             main_image=None,
-            category=Category.objects.create(name='Test Category', slug='test-category'),
-            brand=Brand.objects.create(name='Test Brand', slug='test-brand'),
+            category=Category.objects.create(
+                name='Test Category',
+                slug='test-category',
+                parent=Category.objects.create(name='Parent Category',
+                                               slug='parent-category',
+                                               parent=None,
+                                               image=None, ),
+            ),
+            brand=Brand.objects.create(
+                name='Test Brand',
+                slug='test-brand',
+                description='ghashang',
+                image=None,
+            ),
             discount=Discount.objects.create(code='Test DiscountP', discount_type='percentage', value=50),
             discounted_price=None
         )
@@ -104,3 +115,47 @@ class ProductTestCase(TestCase):
         self.assertEqual(product2.brand.name, 'Test Brand2')
         self.assertEqual(product2.discount.code, 'Test DiscountA')
         self.assertEqual(product2.discounted_price, 900)
+
+class CommentModelTests(TestCase):
+    def setUp(self):
+        self.comment = Comment.objects.create(
+            product = Product.objects.create(
+                name='Test Comment',
+                price=1000,
+                quantity=10,
+                main_image=None,
+                category=Category.objects.create(name='Test Category2', slug='test-category-2'),
+                brand=Brand.objects.create(name='Test Brand2', slug='test-brand-2'),
+                discount=Discount.objects.create(code='Test DiscountA', discount_type='amount', value=100),
+                discounted_price=None
+            ),
+            reply = None,
+            author = User.objects.create(username='zeinab', first_name='zeinab', last_name='moghadamnia', email='zeinab@moghadamnia', phone_number='09032554304'),
+            context = "saaaaaaaalaaaaaam",
+        )
+    def test_comment_model(self):
+        comment = Comment.objects.get(author__email="zeinab@moghadamnia")
+        self.assertEqual(comment.author.phone_number, '09032554304')
+        self.assertEqual(comment.product.name, 'Test Comment')
+        self.assertEqual(comment.context, "saaaaaaaalaaaaaam")
+
+class LikeModelTest(TestCase):
+    def setUp(self):
+        self.like = Like.objects.create(
+            product=Product.objects.create(
+                name='Test Like',
+                price=1000,
+                quantity=10,
+                main_image=None,
+                category=Category.objects.create(name='Test Category2', slug='test-category-2'),
+                brand=Brand.objects.create(name='Test Brand2', slug='test-brand-2'),
+                discount=Discount.objects.create(code='Test DiscountA', discount_type='amount', value=100),
+                discounted_price=None
+            ),
+            user=User.objects.create(username='zeinab', first_name='zeinab', last_name='moghadamnia',
+                                       email='zeinab@moghadamnia', phone_number='09032554304'),
+        )
+    def test_like(self):
+        like = Like.objects.get(product__name='Test Like')
+        self.assertEqual(like.product.discount.discount_type, 'amount')
+        self.assertEqual(like.user.phone_number, '09032554304')
