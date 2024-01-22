adf1a7fa7222eb1690eac3a70b7d9c34551cbadd
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-22 19:08:27 +0330

    [FinalProject][#3]|slug in admin panel updated

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -6,11 +6,57 @@ from .models import Category, Product, Discount, Image, Comment, Like, Brand
 class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ['discounted_price']
     list_display = ('id', 'name', 'category', 'brand', 'quantity', 'price', 'discounted_price', 'is_deleted')
+    fieldsets = [
+        ('Product Information', {
+            'fields': (tuple([
+                'name', 'category', 'brand', 'quantity',
+            ]),),
+        }),
+        ('Additional Information', {
+            'fields': (tuple([
+                'slug', 'description',
+            ]),),
+        }),
+        ('Price Information', {
+            'fields': (tuple([
+                'price', 'discount', 'discounted_price',
+            ]),),
+        }),
+        ('Status Information', {
+            'fields': (tuple([
+                'is_active', 'is_deleted',
+            ]),),
+        }),
+    ]
     search_fields = ('name', 'brand', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'discount_type', 'value', 'activate_at', 'deactivate_at',)
+    fieldsets = [
+        ('Discount Information', {
+            'fields': (tuple([
+                'code', 'discount_type', 'value',
+            ]),),
+        }),
+        ('Activate Information', {
+            'fields': (tuple([
+                'activate_at', 'deactivate_at',
+            ]),),
+        }),
+        ('Status Information', {
+            'fields': (tuple([
+                'is_active', 'is_deleted',
+            ]),),
+        }),
+    ]
+    search_fields = ('code', 'discount_type', 'value')
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'is_active', 'created_at', 'is_deleted')
     search_fields = ("name",)
+    prepopulated_fields = {'slug': ('name',)}
+
 
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug',)
@@ -20,7 +66,7 @@ class BrandAdmin(admin.ModelAdmin):
     filter_horizontal = ()
     prepopulated_fields = {'slug': ('name',)}
     fieldsets = (
-        ('Main', {'fields': ('name', 'slug', 'image',)}),)
+        ('Information', {'fields': ('name', 'slug', 'image',)}),)
 
 
 class CommentAdmin(admin.ModelAdmin):
@@ -30,10 +76,6 @@ class CommentAdmin(admin.ModelAdmin):
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('id', 'product', 'is_main')
-
-
-class DiscountAdmin(admin.ModelAdmin):
-    list_display = ('id', 'discount_type', 'value', 'activate_at', 'deactivate_at')
 
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('id', 'product')
