4b15000710d300189dd5d0ec803ee6d0063e4368
ZeinabMoghadamnia <zeinab.moghadamniaa@gmail.com>
2024-01-31 22:12:10 +0330

    [FinalProject][#5]|permissions completed

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 from ..core.admin import ButtonMixin, CustomExtraButtonsMixin
+from ..accounts.permissions import AccountsAdminPanelPermission
 
 # Register your models here.
 
 @admin.register(Order)
-class OrderAdmin(CustomExtraButtonsMixin, ButtonMixin, admin.ModelAdmin):
+class OrderAdmin(AccountsAdminPanelPermission, CustomExtraButtonsMixin, ButtonMixin, admin.ModelAdmin):
     list_display = ('id', 'user', 'total_price', 'is_paid', 'change_button', 'delete_button')
     list_filter = ('is_paid',)
 
 @admin.register(OrderItem)
-class OrderItemAdmin(CustomExtraButtonsMixin, ButtonMixin, admin.ModelAdmin):
+class OrderItemAdmin(AccountsAdminPanelPermission, CustomExtraButtonsMixin, ButtonMixin, admin.ModelAdmin):
     list_display = ('id',)
